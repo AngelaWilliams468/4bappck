f695b5326933601965100e732b428f70b2ce7e2e
Herman D Cook <110443914+AngelaWilliams468@users.noreply.github.com>
2023-07-28 10:26:08 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@
 DISPLAY_NAME=${DISPLAY_NAME:-'Argo_xray_'}
 
 # 定义 UUID 及 伪装路径,请自行修改.(注意:伪装路径以 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
-UUID=${UUID:-'e4a6c6f8-00c1-41d1-acdd-a5d3296b306d'}
+UUID=${UUID:-'21ef4cb6-051a-4707-a6e2-c12174434e41'}
 VMESS_WSPATH=${VMESS_WSPATH:-'/vmess'}
 VMESS_WARP_WSPATH=${VMESS_WARP_WSPATH:-'/vmess_warp'}
 VLESS_WSPATH=${VLESS_WSPATH:-'/vless'}
